146908e314b92abe8bd97a24f3290dcc75f67d4c
Cy1der <67352978+Cy1der@users.noreply.github.com>
2020-11-20 14:18:22 -0500

    Replaced old files

--- bot/assets/images/commands/foo.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js")
-module.exports= {
-    name: 'foo',
-    category: 'fun',
-    description: 'Foo Bar.',
-    run: async(message, args, client) => {
-        message.channel.send('Bar()')
-    }
-}
--- bot/assets/images/commands/wheek.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js")
-module.exports= {
-    name: 'wheek',
-    category: 'fun',
-    description: 'NEED FOOD',
-    run: async(message, args, client) => {
-        message.channel.send('Wheeeeeeeeeek! 🐹')
-    }
-}
