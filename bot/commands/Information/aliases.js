d4ecac1f0cba22b0544320a6a102b9afff313db6
Cy1der <67352978+Cy1der@users.noreply.github.com>
2020-11-24 14:34:42 -0500

    Update aliases.js

--- bot/commands/Information/aliases.js
+++ bot/commands/Information/aliases.js
@@ -5,16 +5,14 @@ module.exports = {
     maxArgs: 0,
     description: "command aliases",
     run: async (message, args, text, client, prefix, instance) => {
-        const table = new ascii("GuineaBot command aliases:")
-        table.setHeading("Command", "Alias")
-
         let commands = ["cleverbot", "coolkid", "dankmeme", "guineapig", "image", "sarcastic", "wholesomememe", "connect4", "guessnum", "hangman", "minesweeper", "banfilth", "deepfry", "addlevel", "addxp", "createuserxp", "deleteuserxp", "leaderboard", "rank", "removelevel", "removexp", "required", "chatmute", "deafen", "removewarn", "setnick", "unchatmute", "undeafen", "unvoicemute", "voicemute", "warnings", "connect", "disconnect", "lyrics", "nowplaying", "play", "playlist", "queue", "volume", "createtextchannel", "createvoicechannel", "deletevoicechannel", "deletetextchannel", "avatar", "google", "randomnumber", "randomnumberrange", "temporary", "clear", "technicalindicator", "sectorperformance"]
         let aliases = ["clever", "cool", "dm", "pig", "i", "sarc", "wm", "c4", "guess", "hang", "sweeper", "filth", "fry", "+lvl", "+xp", "cuxp", "duxp", "lb", "level", "-lvl", "-xp", "req", "cm", "deaf", "-warn", "nick", "ucm", "undeaf", "uvm", "vm", "warns", "join", "dc", "ly", "np", "p", "pl", "q", "v", "ctc", "cvc", "dvc", "dtc", "pfp", "ggl", "rn", "rnr", "temp", "purge", "technical", "sector"]
+        let str = ""
 
         for (let i = 0; i < commands.length; i++) {
-            table.addRow(commands[i], aliases[i])
+            str += `${commands[i]} - ${aliases[i]}\n`
         }
-
-        message.channel.send(`\`\`\`\n${table.toString()}\n\`\`\``)
+        
+        message.channel.send(`\`\`\`\n${str}\n\`\`\``)
     }
 }
